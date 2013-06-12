b15cab68b41eb1ffea03f03ae85fdc60a7f21ddd
DQNEOO <DQNEOO@gmail.com>
2013-06-12 17:15:01 +0000

    sync with develop

--- class/Plugin/Validator/Min.php
+++ class/Plugin/Validator/Min.php
@@ -1,7 +1,7 @@
 <?php
 // vim: foldmethod=marker
 /**
- *  Ethna_Plugin_Validator_Min.php
+ *  Min.php
  *
  *  @author     ICHII Takashi <ichii386@schweetheart.jp>
  *  @license    http://www.opensource.org/licenses/bsd-license.php The BSD License
@@ -20,7 +20,7 @@
 class Ethna_Plugin_Validator_Min extends Ethna_Plugin_Validator
 {
     /** @var    bool    配列を受け取るかフラグ */
-    var $accept_array = false;
+    public $accept_array = false;
 
     /**
      *  最小値のチェックを行う
@@ -30,7 +30,7 @@ class Ethna_Plugin_Validator_Min extends Ethna_Plugin_Validator
      *  @param  mixed   $var        フォームの値
      *  @param  array   $params     プラグインのパラメータ
      */
-    function validate($name, $var, $params)
+    public function validate($name, $var, $params)
     {
         $true = true;
         $type = $this->getFormType($name);
@@ -103,4 +103,3 @@ class Ethna_Plugin_Validator_Min extends Ethna_Plugin_Validator
     }
 }
 // }}}
-
