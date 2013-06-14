f3efa59936354e5861b798fcddcb3f9197a64612
DQNEO <DQNEOO@gmail.com>
2013-06-14 14:02:58 +0900

    refactor

--- class/Plugin/Smarty/SmartyPlugin.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once ETHNA_BASE . '/class/Plugin/Smarty/block.form.php';
-require_once ETHNA_BASE . '/class/Plugin/Smarty/function.form_input.php';
-require_once ETHNA_BASE . '/class/Plugin/Smarty/modifier.checkbox.php';
-require_once ETHNA_BASE . '/class/Plugin/Smarty/modifier.truncate_i18n.php';
-require_once ETHNA_BASE . '/class/Plugin/Smarty/modifier.unique.php';
-require_once ETHNA_BASE . '/class/Plugin/Smarty/modifier.wordwrap_i18n.php';
