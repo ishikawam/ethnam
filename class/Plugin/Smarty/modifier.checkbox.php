011366e67d0e4cb59395fa2096b889160d44dcc4
DQNEOO <DQNEOO@gmail.com>
2013-06-16 06:35:53 +0000

    sync develop

--- class/Plugin/Smarty/modifier.checkbox.php
+++ class/Plugin/Smarty/modifier.checkbox.php
@@ -9,15 +9,16 @@
  *  </code>
  *  <code>
  *  <input type="checkbox" name="test">
- *  <input type="checkbox" name="test" checkbox>
+ *  <input type="checkbox" name="test" checked="checked">
  *  </code>
  *
- *  @param  string  $string チェックボックスに渡されたフォーム値
- *  @return string  $stringが空文字列あるいは0以外の場合は"checked"
+ *  @param  string  $string チェックボックスに渡されたフォーム値(スカラーのみ)
+ *  @return string  $stringが空文字列あるいは0, null, false 以外の場合は"checked"
  */
 function smarty_modifier_checkbox($string)
 {
-    if ($string != "" && $string != 0) {
-        return "checked";
+    if (is_scalar($string) && $string != "" && $string != "0") {
+        return 'checked="checked"';
     }
 }
+
